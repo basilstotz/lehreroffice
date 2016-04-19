bf7c0e6170c596ca1f3746717bf5077c4ff7903d
root <root@bootserver.amxa.opinys.fi>
2016-04-19 10:46:30 +0200

    ddddddddd

---
+++ clean.ch
@@ -0,0 +1,1 @@
+rm -r lehreroffice-1.0/opt/lehreroffice/*
--- extract.sh
+++ extract.sh
@@ -1,4 +1,4 @@
-if ! test -d opt/lehreroffice; then
-  mkdir -p opt/lehreroffice
+if ! test -d lehreroffice-1.0/opt/lehreroffice; then
+  mkdir -p lehreroffice-1.0/opt/lehreroffice
 fi
-unzip  *.zip -d opt/lehreroffice
+unzip  *.zip -d lehreroffice-1.0/opt/lehreroffice
