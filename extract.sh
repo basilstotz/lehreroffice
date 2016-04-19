b0721eae76126b190c4f077bfd0dcb8957be945e
root <root@bootserver.amxa.opinys.fi>
2016-04-19 21:17:22 +0200

    fixed extract.sh and perms

--- extract.sh
+++ extract.sh
@@ -2,3 +2,4 @@ if ! test -d lehreroffice-1.0/opt/lehreroffice; then
   mkdir -p lehreroffice-1.0/opt/lehreroffice
 fi
 unzip  *.zip -d lehreroffice-1.0/opt/lehreroffice
+chmod +x lehreroffice-1.0/opt/lehreroffice/LehrerOffice
